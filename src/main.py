4735901e5fe5f54cbcbf4a69eef65ee04b490bea
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-06-06 03:49:43 +0100

    added functionality to the main

--- src/main.py
+++ src/main.py
@@ -6,3 +6,28 @@ def show_menu():
     print("2. List Task")
     print("3. Mark Done Task")
     print("4. EXIT")
+
+def main():
+    while True:
+        show_menu()
+        choice = input('Choose an option please')
+
+        if choice == 1:
+            task = print('Enter a task')
+            add_task(task)
+        elif choice == 2:
+            list_tasks()
+        elif choice == 3:
+            try:
+                index = print('what task would you like to mark as completed') -1
+                mark_done(index)
+            except ValueError:
+                print('please enter a valid task number')
+        elif choice == 4:
+            print('see you later!')
+            break
+        else:
+            print('invalid choice, try again')
+
+if __name__ == '__main__':
+    main()
