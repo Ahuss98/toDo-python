d678390d3febdc5e73728d8a33be09b74807b935
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-06-06 03:20:15 +0100

    load tasks function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # toDo-python
+
+
+# the todo function
+- the load_tasks
+    - 1. checks if a file containing saved tasks exists
+    - 2. if it does, read and return the tasks from the file
+    - 3. if not return an empty list
--- src/todo.py
+++ src/todo.py
@@ -2,3 +2,10 @@ import json
 from pathlib import Path
 
 DATA_FILE = Path("data/tasks.json")
+
+
+def load_tasks():
+    if DATA_FILE.exists():
+        with open(DATA_FILE,'r') as f:
+            return json.load(f)
+    return []
